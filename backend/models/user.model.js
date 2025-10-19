6c27222dbee2897931a67a0193710a4da15414f7
Amit Mehta <amitmehta0510@gmail.com>
2025-10-19 21:58:31 +0530

    First commit, Creates models

---
+++ backend/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: trusted,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    default: "default.jpg",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  token: {
+    type: String,
+    default: "",
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+export default User;
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import postsRoutes from "./routes/posts.routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(postsRoutes);
+
+const start = async () => {
+  const connectDB = await mongoose.connect(process.env.MONGODB_URI);
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+};
+
+start();
